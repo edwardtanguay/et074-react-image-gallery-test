9396c466b9422eb4155e6a97bbcbbe9230d3b35f
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-08 10:29:06 +0200

    basic gallery works

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.scss';
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>React Image Gallery Test</h1>
+      <h1>React Image Gallery Test</h1>
+      <ImageGallery items={images} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
