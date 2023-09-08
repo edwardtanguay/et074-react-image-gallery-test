eb766c6b133cda2784980f285a6ef0bb476718ce
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-08 12:47:27 +0200

    all empoyees

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,30 @@ import './App.scss';
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import employees from './data/employees.json';
 
-  const images = [
-    {
-      original: 'images/employee_1.jpg',
-      description: 'Jill',
-    },
-    {
-      original: 'images/employee_2.jpg',
-      description: 'Alex',
-    },
-  ];
+const  images = employees.map(m => ({original: `images/employee_${m.employeeID}.jpg`, description: m.firstName}))
 
 const customRenderItem = (item) => {
-  return (
-    <div className="custom-gallery-item">
-      <img src={item.original} alt={item.description} />
-      {item.description && (
-        <span className="image-description">{item.description}</span>
-      )}
-    </div>
-  );
+	return (
+		<div className="custom-gallery-item">
+			<img src={item.original} alt={item.description} />
+			{item.description && (
+				<span className="image-description"><span>{item.description}</span></span>
+			)}
+		</div>
+	);
 };
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>React Image Gallery Test</h1>
-      <ImageGallery items={images} renderItem={customRenderItem} />
-    </>
-  );
+	return (
+		<>
+			<h1>React Image Gallery Test</h1>
+			<ImageGallery items={images} renderItem={customRenderItem} />
+		</>
+	);
 }
 
 export default App;
