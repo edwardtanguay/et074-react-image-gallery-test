bbc7d20165befb8500c144f23964b4949994fda0
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-08 11:33:17 +0200

    employee photos in

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,15 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
-// const images = [
-//   {
-//     original: "https://picsum.photos/id/1018/1000/600/",
-//     thumbnail: "https://picsum.photos/id/1018/250/150/",
-//   },
-//   {
-//     original: "https://picsum.photos/id/1015/1000/600/",
-//     thumbnail: "https://picsum.photos/id/1015/250/150/",
-//   },
-//   {
-//     original: "https://picsum.photos/id/1019/1000/600/",
-//     thumbnail: "https://picsum.photos/id/1019/250/150/",
-//   },
-// ];
-
   const images = [
     {
-      original: 'https://picsum.photos/id/1018/1000/600/',
-      description: 'Description for Image 1',
+      original: 'images/employee_1.jpg',
+      description: 'Jill',
     },
     {
-      original: 'https://picsum.photos/id/1015/1000/600/',
-      description: 'Description for Image 2',
+      original: 'images/employee_2.jpg',
+      description: 'Alex',
     },
-    // Add more images as needed
   ];
 
 const customRenderItem = (item) => {
