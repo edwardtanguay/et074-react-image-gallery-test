5961ae53d909e7466367d81767d7603ce10730df
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-08 10:36:15 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,43 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
-const images = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
+// const images = [
+//   {
+//     original: "https://picsum.photos/id/1018/1000/600/",
+//     thumbnail: "https://picsum.photos/id/1018/250/150/",
+//   },
+//   {
+//     original: "https://picsum.photos/id/1015/1000/600/",
+//     thumbnail: "https://picsum.photos/id/1015/250/150/",
+//   },
+//   {
+//     original: "https://picsum.photos/id/1019/1000/600/",
+//     thumbnail: "https://picsum.photos/id/1019/250/150/",
+//   },
+// ];
+
+  const images = [
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      description: 'Description for Image 1',
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      description: 'Description for Image 2',
+    },
+    // Add more images as needed
+  ];
+
+const customRenderItem = (item) => {
+  return (
+    <div className="custom-gallery-item">
+      <img src={item.original} alt={item.description} />
+      {item.description && (
+        <span className="image-description">{item.description}</span>
+      )}
+    </div>
+  );
+};
 
 function App() {
   const [count, setCount] = useState(0);
@@ -25,7 +48,7 @@ function App() {
   return (
     <>
       <h1>React Image Gallery Test</h1>
-      <ImageGallery items={images} />
+      <ImageGallery items={images} renderItem={customRenderItem} />
     </>
   );
 }
